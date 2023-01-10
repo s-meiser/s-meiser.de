01265f49871558382821f135a2d5abc952ea1736
Sebastian <Sebastian.Meiser@gmail.com>
2023-01-10 21:16:53 +0100

    update workflow

--- app/assets/controllers/hello_controller.js
+++ app/assets/controllers/hello_controller.js
@@ -12,5 +12,6 @@ import { Controller } from '@hotwired/stimulus';
 export default class extends Controller {
     connect() {
         this.element.textContent = 'Hello Stimulus! Edit me in assets/controllers/hello_controller.js';
+        console.log('test for github action workflow');
     }
 }

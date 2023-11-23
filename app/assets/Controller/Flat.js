bfeb51a23fe9bdbf2b79a8bfbe70cb44d512bce8
Sebastian <Sebastian.Meiser@gmail.com>
2023-11-23 18:24:49 +0100

    [Task] Setup html/css transitions

--- app/assets/Controller/Flat.js
+++ app/assets/Controller/Flat.js
@@ -15,6 +15,12 @@ export default class extends Controller {
      * @param event
      */
     navbar(event) {
-        console.log(event)
+        console.log(event.target);
+        const navbarToggle = document.querySelector('.navbar-container')
+        navbarToggle.classList.toggle('show');
+        navbarToggle.classList.toggle('column-4');
+
+        const navbarGridEffect = document.querySelector('.navbar-grid-effect')
+        navbarGridEffect.classList.toggle('grid-effect-show');
     }
 }
